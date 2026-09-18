5c34bbd46a4a7b07379bfaaeca4bce2a0c660c8f
Mohnish Lad <mohnishlad@gmail.com>
2026-09-18 01:44:10 -0700

    state file management

--- infra/terraform/versions.tf
+++ infra/terraform/versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "remote" {
+    organization = "mohnishlad"
+    workspaces {
+      name = "fastly"
+    }
+  }
+
   required_providers {
     fastly = {
       source  = "fastly/fastly"
