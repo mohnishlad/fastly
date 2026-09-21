5b874a8e87c9a6a26a442b3badfe1eac19ea10ea
Mohnish Lad <mohnishlad@gmail.com>
2026-09-21 02:53:10 -0700

    txt output

--- infra/fastly/outputs.tf
+++ infra/fastly/outputs.tf
@@ -17,3 +17,14 @@ output "origin_hostname" {
   description = "Hostname used as the S3 origin for the site."
   value       = var.origin_hostname
 }
+
+output "tls_challenge_name" {
+  description = "TXT record host name Fastly requires for ACME DNS validation."
+  value       = try([for challenge in fastly_tls_subscription.www.managed_dns_challenges : challenge.record_name][0], null)
+}
+
+output "tls_challenge_value" {
+  description = "TXT record value Fastly requires for ACME DNS validation."
+  value       = try([for challenge in fastly_tls_subscription.www.managed_dns_challenges : challenge.record_value][0], null)
+  sensitive   = true
+}
