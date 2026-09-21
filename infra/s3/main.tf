07e8cba5916b4a1e77f0ac6f8b2596bf1a810913
Mohnish Lad <mohnishlad@gmail.com>
2026-09-21 01:45:40 -0700

    separate workflows

--- infra/s3/main.tf
+++ infra/s3/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "site" {
   bucket        = var.bucket_name
-  force_destroy = true
+  force_destroy = false
 }
 
 resource "aws_s3_bucket_website_configuration" "site" {
